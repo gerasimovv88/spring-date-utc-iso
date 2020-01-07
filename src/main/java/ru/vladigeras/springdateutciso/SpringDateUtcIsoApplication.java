7e729b2c202ed46fe56b9e317aae7cd17ae2983d
Vladislav Gerasimov <gerasimov.v88@gmail.com>
2020-01-07 13:04:57 +0300

    Initial commit

---
+++ src/main/java/ru/vladigeras/springdateutciso/SpringDateUtcIsoApplication.java
@@ -0,0 +1,13 @@
+package ru.vladigeras.springdateutciso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDateUtcIsoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDateUtcIsoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/vladigeras/springdateutciso/SpringDateUtcIsoApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vladigeras.springdateutciso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDateUtcIsoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
